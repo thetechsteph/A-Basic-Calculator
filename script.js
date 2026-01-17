a91f9aeebb3bc391cc6426b7ab0a00f03f19ba73
superstephgreat <superstephdev@gmail.com>
2026-01-17 13:38:23 +0100

    bodmas

--- script.js
+++ script.js
@@ -13,47 +13,80 @@ const equalsBtn = document.querySelector('.equals');
 let expression =[];
 let currentValue = '';
 
-const updateLivePreview = () => {
+
+
+
+const precedence ={
+  '+': 1,
+  '-': 1,
+  '×': 2,
+  '÷': 2,
+  '%': 2,
+};
+
+
+function toPostFix(expr) {
+  let output = [];
+  let stack = [];
   
-  if(expression.length === 0 || expression.length===1){
-    resultDisplay.innerText = '';
-    return
-  }
-  const lastItem = expression[expression.length - 1]
-  if(isNaN(lastItem) ){
-    resultDisplay.innerText =''
-    return 
-  }
-  let result = expression.reduce((acc, item, index) => {
-    if(index === 0) return Number(item);
-    
-    const operator = expression[index - 1];
-    
-    const number = Number(item);
-    
-    if (operator === '+') {
-      return acc + number;
+  for(let token of  expr){
+    if (!isNaN(token)){
+      output.push(token);
+    } else{
+      
+      while(stack.length && precedence[stack[stack.length - 1]] >= precedence[token]) {
+        output.push(stack.pop())
+      }
+      stack.push(token);
     }
-    if (operator === '-') {
-      return acc - number;
+  }
+  while(stack.length){
+    output.push(stack.pop());
+  }
+  return output;
+}
+ 
+function evaluatePostfix(postfix) {
+  let stack = [];
+
+  for (let token of postfix) {
+    if (!isNaN(token)) {
+      stack.push(Number(token));
+    } else {
+      let b = stack.pop();
+      let a = stack.pop();
+
+      if (token === '+') stack.push(a + b);
+      if (token === '-') stack.push(a - b);
+      if (token === '×') stack.push(a * b);
+      if (token === '÷') stack.push(a / b);
+      if (token === '%') stack.push(a % b);
     }
-    if (operator === '÷') {
-  return acc / number;
-   }
-  if (operator === '×') {
-    return acc * number;
   }
-  if(operator === '%'){
-    return acc % number;
-  }
-  
-  return acc
-  });
-  resultDisplay.innerText = result;
+
+  return stack[0];
 }
 
 
+const updateLivePreview = () => {
+  if (expression.length === 0 || expression.length===1) {
+    resultDisplay.innerText = '';
+    return;
+  }
 
+  const lastItem = expression[expression.length - 1];
+
+
+  if (isNaN(lastItem)) {
+    resultDisplay.innerText = '';
+    return;
+  }
+
+  const postfix = toPostFix(expression);
+  const result = evaluatePostfix(postfix);
+
+  resultDisplay.innerText = result;
+}
 numberBtns.forEach(button => {
   button.addEventListener('click', ()=> {
     const buttonValue = button.innerText;
@@ -104,45 +137,22 @@ operatorBtns.forEach(button => {
 
 
 equalsBtn.addEventListener('click', () => {
-  
   if (expression.length === 0) return;
-  
-  const lastItem = expression[expression.length -1]
- if(isNaN(lastItem))return;
-  
-  let result = expression.reduce((acc, item, index) => {
-    if(index === 0) return Number(item);
-    
-    const operator = expression[index - 1];
-    
-    const number = Number(item);
-    
-    if (operator === '+') {
-      return acc + number;
-    }
-    if (operator === '-') {
-      return acc - number;
-    }
-    if (operator === '÷') {
-  return acc / number;
-   }
-  if (operator === '×') {
-    return acc * number;
-  }
-  if(operator === '%'){
-    return acc % number;
-  }
-  
-  return acc
-  });
-  
+
+  const lastItem = expression[expression.length - 1];
+  if (isNaN(lastItem)) return;
+
+  const postfix = toPostFix(expression);
+  const result = evaluatePostfix(postfix);
+
   previousOperand.innerText = result;
-  currentOperand.innerText =''
-  resultDisplay.innerText = ''
-  
-  expression =[String(result)];
-  
+  currentOperand.innerText = '';
+  resultDisplay.innerText = '';
+  expression = [String(result)];
 });
+
+
+
 
 controlBtns.forEach(button => {
   button.addEventListener('click', () => {
